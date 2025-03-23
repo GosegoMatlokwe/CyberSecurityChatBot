223ca25a053f4e4d3a86610c8f7496a322d79c4d
GosegoMatlokwe <166373638+GosegoMatlokwe@users.noreply.github.com>
2025-03-23 04:27:57 +0200

    Added a greeting class

---
+++ CyberSecurityChatBot/Greeting.cs
@@ -0,0 +1,15 @@
+﻿using System.Media;
+using System;
+
+namespace CyberSecurityChatBot
+{
+  public class Greeting
+    {
+        public Greeting()
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\Users\goseg\OneDrive\Documents\GitHub\CyberSecurityChatBot\CyberSecurityChatBot\CyberSecurityAwarenessChatbotGreeting.wav");
+            player.PlaySync();
+            Console.WriteLine("Hello! Welcome to the Cybersecurity Awareness ChatBot. I am here to help you stay safe online");
+        }
+    }
+}
