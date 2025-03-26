160d2704967ff8eb2f1b44899d142a82ae0a84ad
GosegoMatlokwe <166373638+GosegoMatlokwe@users.noreply.github.com>
2025-03-26 03:28:01 +0200

    Updated the greeting class to display the ASCII Art

--- CyberSecurityChatBot/Greeting.cs
+++ CyberSecurityChatBot/Greeting.cs
@@ -9,6 +9,25 @@ namespace CyberSecurityChatBot
         {
             SoundPlayer player = new SoundPlayer(@"C:\Users\goseg\OneDrive\Documents\GitHub\CyberSecurityChatBot\CyberSecurityChatBot\CyberSecurityAwarenessChatbotGreeting.wav");
             player.PlaySync();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(@"                                                                                                                                                                                                                                   
+     _/_/_/            _/                              _/_/_/                                          _/    _/                      _/_/                                                                                              
+  _/        _/    _/  _/_/_/      _/_/    _/  _/_/  _/          _/_/      _/_/_/  _/    _/  _/  _/_/      _/_/_/_/  _/    _/      _/    _/  _/      _/      _/    _/_/_/  _/  _/_/    _/_/    _/_/_/      _/_/      _/_/_/    _/_/_/   
+ _/        _/    _/  _/    _/  _/_/_/_/  _/_/        _/_/    _/_/_/_/  _/        _/    _/  _/_/      _/    _/      _/    _/      _/_/_/_/  _/      _/      _/  _/    _/  _/_/      _/_/_/_/  _/    _/  _/_/_/_/  _/_/      _/_/        
+_/        _/    _/  _/    _/  _/        _/              _/  _/        _/        _/    _/  _/        _/    _/      _/    _/      _/    _/    _/  _/  _/  _/    _/    _/  _/        _/        _/    _/  _/            _/_/      _/_/     
+ _/_/_/    _/_/_/  _/_/_/      _/_/_/  _/        _/_/_/      _/_/_/    _/_/_/    _/_/_/  _/        _/      _/_/    _/_/_/      _/    _/      _/      _/        _/_/_/  _/          _/_/_/  _/    _/    _/_/_/  _/_/_/    _/_/_/        
+              _/                                                                                                      _/                                                                                                               
+         _/_/                                                                                                    _/_/                                                                                                                  
+                                                                          
+     _/_/_/  _/                    _/      _/_/_/                _/       
+  _/        _/_/_/      _/_/_/  _/_/_/_/  _/    _/    _/_/    _/_/_/_/    
+ _/        _/    _/  _/    _/    _/      _/_/_/    _/    _/    _/         
+_/        _/    _/  _/    _/    _/      _/    _/  _/    _/    _/          
+ _/_/_/  _/    _/    _/_/_/      _/_/  _/_/_/      _/_/        _/_/  _/   
+                                                                          
+                                                                          
+");
+            Console.ResetColor();
             Console.WriteLine("Hello! Welcome to the Cybersecurity Awareness ChatBot. I am here to help you stay safe online");
         }
     }
